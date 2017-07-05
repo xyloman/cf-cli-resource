a7cb07d0fce512f7c947c20bf968dc59e526e281
bk016677 <bryan.kelly@cerner.com>
2017-07-04 21:22:59 -0500

    cleaned up cf_get_service_guid logic.

--- assets/cf-functions.sh
+++ assets/cf-functions.sh
@@ -156,12 +156,7 @@ function cf_service_exists() {
 
 function cf_get_service_guid() { 
   local service_instance=$1
-  local guid=$(cf service $service_instance --guid)
-  if [[ -z $(echo $guid | grep 'FAILED') ]]; then 
-    echo ""
-  else
-    echo "$guid"
-  fi
+  service_guid=$(cf service $service_instance --guid)
 }
 
 function cf_create_service() {
